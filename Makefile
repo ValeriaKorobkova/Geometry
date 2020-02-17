0141f74c279216c00bc702757ea78513b0b19691
Коробкова Валерия Владимировна <p915s12@awp02.csc.local>
2020-02-17 13:09:07 +0700

    Setup formatter

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 build: main.c
 	g++ -Wall main.c -o geometry
+
+format: *.c
+	clang-format -i *.c
--- main.c
+++ main.c
@@ -3,50 +3,50 @@
 #define PI 3.141592653589793
 
 struct Circle {
-	int x;
-	int y;
-	float radius;
+    int x;
+    int y;
+    float radius;
 };
 
 Circle define_circle();
 float get_area(Circle circle);
 
-int main() {
-	int size = 3;
-	Circle circles[size];
-	for (int i = 0; i < size; i++) {
-		circles[i] = define_circle();
-	}
+int main()
+{
+    int size = 3;
+    Circle circles[size];
+    for (int i = 0; i < size; i++) {
+        circles[i] = define_circle();
+    }
 
-	for (int i = 0; i < size; i++) {
-		printf("circle(%d %d, %.2f)\n", 
-			circles[i].x,
-		       	circles[i].y,
-		       	circles[i].radius
-		);
-		printf(" area = %.2f\n",get_area(circles[i]));
+    for (int i = 0; i < size; i++) {
+        printf("circle(%d %d, %.2f)\n",
+               circles[i].x,
+               circles[i].y,
+               circles[i].radius);
+        printf(" area = %.2f\n", get_area(circles[i]));
+    }
 
-        }
-	
-
-	return 0;
+    return 0;
 }
 
-Circle define_circle() {
-	Circle circle;
+Circle define_circle()
+{
+    Circle circle;
 
-	printf("Enter x, y, radius: ");
-	int x, y;
-	float radius;
-	scanf("%d %d %f", &x, &y, &radius);
+    printf("Enter x, y, radius: ");
+    int x, y;
+    float radius;
+    scanf("%d %d %f", &x, &y, &radius);
 
-	circle.x = x;
-	circle.y = y;
-	circle.radius = radius;
+    circle.x = x;
+    circle.y = y;
+    circle.radius = radius;
 
-	return circle;
+    return circle;
 }
 
-float get_area(Circle circle){
-	return PI*circle.radius*circle.radius;
+float get_area(Circle circle)
+{
+    return PI * circle.radius * circle.radius;
 }
