3c4e3238fad96ff016dc4dbd0ca78632525a20bd
Коробкова Валерия Владимировна <p915s12@awp02.csc.local>
2020-02-29 11:04:45 +0700

    Implemented intersections

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 
 #define PI 3.141592653589793
@@ -11,6 +12,7 @@ struct Circle {
 Circle define_circle();
 float get_area(Circle circle);
 float get_perimeter(Circle circle);
+int is_intersected(Circle circle_1, Circle circle_2);
 
 int main()
 {
@@ -19,6 +21,7 @@ int main()
     for (int i = 0; i < size; i++) {
         circles[i] = define_circle();
     }
+    printf("\n\n");
 
     for (int i = 0; i < size; i++) {
         printf("circle(%d %d, %.2f)\n",
@@ -26,7 +29,23 @@ int main()
                circles[i].y,
                circles[i].radius);
         printf(" area = %.2f\n", get_area(circles[i]));
-        printf(" perimeter = %.2f\n\n", get_perimeter(circles[i]));
+        printf(" perimeter = %.2f\n", get_perimeter(circles[i]));
+        printf(" intersects: \n");
+
+        int flag = 0;
+        for (int j = 0; j < size; j++) {
+            if (i == j)
+                continue;
+            if (is_intersected(circles[i], circles[j])) {
+                printf("  %d. circle\n", j);
+                flag = 1;
+            }
+        }
+        if (!flag) {
+            printf("  no, sorry :(\n");
+        }
+
+        printf("\n");
     }
 
     return 0;
@@ -56,4 +75,15 @@ float get_perimeter(Circle circle)
 float get_area(Circle circle)
 {
     return PI * circle.radius * circle.radius;
+}
+
+int is_intersected(Circle circle_1, Circle circle_2)
+{
+    float distance
+            = sqrt(pow((circle_1.x - circle_2.x), 2)
+                   + pow((circle_1.y - circle_2.y), 2));
+    if (circle_1.radius + circle_2.radius >= distance)
+        return 1;
+    else
+        return 0;
 }
